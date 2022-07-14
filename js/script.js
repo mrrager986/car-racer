1a2c0ca070da88f252cf0cedb168fa78a13c11de
mrrager986 <90148540+mrrager986@users.noreply.github.com>
2022-07-14 17:41:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,77 +1,7 @@
 // script.js
 
 $(function() {
-     
-    $("button").click(function() {
-        $('#box').fadeOut(1000);
-    });
-    
-    /* -----------------
-       jQUERY SELECTORS
-       ----------------- */
-    
-    // Grouping selector
-    // $('h2,h3,h4').css('border','solid 1px red');
-    
-    // ID selector
-    // $('div#container').css('border','solid 1px red');
-    
-    // Class selector
-    // $('p.lead').css('border','solid 1px red');
-    
-    // Pseudo-element selector - selects first element
-    // $('li:first').css('border','solid 1px red');
-    
-    // Pseudo-element selector - selects even P tags
-    // $('p:even').css('border','solid 1px red');
-    
-    // Descendant selector
-    // $('div em').css('border','solid 1px red');
-    
-    // Child selector
-    // $('div > p').css('border','solid 1px red');
-    
-    // jQuery Header selector
-    // $(':header').css('border','solid 1px red');
-    
-    // jQuery Contains selector
-    // $('div:contains("Brad")').css('border','solid 1px red');
-    
-    /* --------------------
-       jQUERY EVENT METHODS
-       -------------------- */
-    
-    $('#box').click(function() {
-        alert('you just clicked the box');
-    });
-    
-    $("input").blur(function() {
-        
-        if( $(this).val() == "" ) {
-            $(this).css('border','solid 1px red');
-            $('#box').text('Forgot to add text?');
-        }
-        
-    });
-    
-    $("input").keydown(function() {
-        
-        if( $(this).val() !== "" ) {
-            $(this).css('border','solid 1px #777');
-            $('#box').text('Thanks for that!');
-        }
-        
-    });
-    
-    $('#box').hover(function() {
-        
-        $(this).text('you hovered in!');
-        
-    }, function() {
-        
-        $(this).text('you hovered out!');
-        
-    });
+   
     
     
     // Click the GO button
